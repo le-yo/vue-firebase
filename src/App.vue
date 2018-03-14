25b9adfa0708e21a52c306b298412d9b213092d5
Le-Yo <le-yo@le-yo.com>
2018-03-14 15:03:52 +0300

    added form

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>VueFirebase</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input class="form-control" type="text">
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input class="form-control" type="text">
+                </div>
+                <button class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
